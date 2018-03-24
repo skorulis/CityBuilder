44d65ef5dcde8c813ad11e307bea6875c3bbc069
Alex Skorulis <skorulis@gmail.com>
2018-03-24 11:06:02 +1100

    Initial Commit

---
+++ CityBuilder/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  CityBuilder
+//
+//  Created by Alexander Skorulis on 24/3/18.
+//  Copyright © 2018 Alex Skorulis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
---
+++ CityBuilderTests/CityBuilderTests.swift
@@ -0,0 +1,36 @@
+//
+//  CityBuilderTests.swift
+//  CityBuilderTests
+//
+//  Created by Alexander Skorulis on 24/3/18.
+//  Copyright © 2018 Alex Skorulis. All rights reserved.
+//
+
+import XCTest
+@testable import CityBuilder
+
+class CityBuilderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
